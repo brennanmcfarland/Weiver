b2e584c96648cf5f3876b776245cc114c5666359
Josh Reichman <techfreak101@users.noreply.github.com>
2016-10-08 15:31:02 -0400

    Add files via upload

---
+++ KHE2016/src/ParseCnet.java
@@ -0,0 +1,42 @@
+package khe2016;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+public class ParseCnet implements ParseWebPage {
+
+	public ParseCnet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void findArticleBody() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findSubtitle() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findPros() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void findCons() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ KHE2016/src/ParseWebPage.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package khe2016;
+
+/**
+ * @author joshreichman
+ *
+ */
+public interface ParseWebPage {
+void findArticleBody();
+void findSubtitle();
+void findPros();
+void findCons();
+}
