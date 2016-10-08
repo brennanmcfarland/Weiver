7ccdfb4428a91c9985af55697ea8280972abcaa2
Emilio Lopez <eil11@case.edu>
2016-10-08 18:51:59 -0400

    findPros and findCons work. They no longer include the words "The Good" and "The Bad" respectively. More importantly, the findPros only outputs the Pros section and vice versa.

--- KHE2016/src/ParseCnet.java
+++ KHE2016/src/ParseCnet.java
@@ -1,6 +1,6 @@
 
 import java.io.IOException;
-
+import java.util.regex.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -38,16 +38,45 @@ public class ParseCnet implements ParseWebPage {
 
     public String findPros(Document d) throws IOException {
         // TODO Auto-generated method stub
-        String title = d.select("div.quickinfo > *").toString();
+        StringBuilder sb = new StringBuilder();
+        String title = d.select("div.quickinfo > *").toString();        
         title = title.replaceAll("\\<.*?>", "");
-        return("Pros: " + title); 
+        title = title.substring(8);
+        
+        // The Good section of a review is separated by a new line 
+        for (int i = 0; i < title.length(); i++){
+            if (title.charAt(i) != '\n')
+                sb.append(title.charAt(i));
+            else
+                break;
+        }
+        
+        return("Pros: " + sb.toString()); 
     }
 
     public String findCons(Document d) throws IOException {
         // TODO Auto-generated method stub
-        String title = d.select("div.quickinfo > *").toString();
+        int newLineCounter = 0;
+        
+        StringBuilder sb = new StringBuilder();
+        String title = d.select("div.quickinfo > *").toString();        
         title = title.replaceAll("\\<.*?>", "");
-        return("Cons: " + title); 
+        
+        
+        /* Filters the Bad part of the summary from the Good and Bottomline*/
+        for (int i = 0; i < title.length(); i++){
+            
+            if (newLineCounter == 1)
+                sb.append(title.charAt(i));
+                        
+            if (title.charAt(i) == '\n')
+                newLineCounter++;
+            
+            if (newLineCounter > 1)
+                break;            
+        }        
+        String returnString = sb.toString().substring(7);
+        return("Cons: " + returnString); 
     }
     
     public static void main(String[] args) throws IOException, IllegalArgumentException {
