fab035f2ba7ba361b26570ba9633ccecf6c3c00e
Emilio Lopez <eil11@case.edu>
2016-10-08 11:51:08 -0400

    WebPage contains Pros and Cons fields

--- KHE2016/src/WebPage.java
+++ KHE2016/src/WebPage.java
@@ -2,24 +2,44 @@
 /**
  * Abstract class that allows for standardization of fields for the unique
  * structure of each publisher's HTML structure.
- * @author Emilio Lopez
- * *
+ *
+ * @author Emilio Lopez *
  */
 public abstract class WebPage {
 
-    /** Web site publisher of the review. */
+    /**
+     * Web site publisher of the review.
+     */
     private String publisher;
 
-    /** Article title. */
+    /**
+     * Article title.
+     */
     private String articleTitle;
 
-    /** The author of the article. */
+    /**
+     * The author of the article.
+     */
     private String author;
 
-    /** The review article body. */
+    /**
+     * The review article body.
+     */
     private String articleBody;
 
-    /** The URL of the review. */
+    /**
+     * The URL of the review.
+     */
     private String URL;
-    
+
+    /**
+     * An array containing the list of pros for a product.
+     */
+    private String[] pros;
+
+    /**
+     * An array containing the list of cons for a product. 
+     */
+    private String[] cons;
+
 }
