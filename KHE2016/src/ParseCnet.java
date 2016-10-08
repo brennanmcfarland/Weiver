e10f4a6ce106e1a5d72224f44bf290cd52bda48e
Josh Reichman <joshreichman@JoshMBP.local>
2016-10-08 15:41:50 -0400

    ParseWebPage Correction
    Forgot to add code

--- KHE2016/src/ParseCnet.java
+++ KHE2016/src/ParseCnet.java
@@ -1,29 +1,45 @@
-package khe2016;
+import java.io.IOException;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-public class ParseCnet implements ParseWebPage {
+import java.util.Scanner;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-	public ParseCnet() {
+/**
+ *
+ * @author joshreichman
+ */
+public class ParseCnet implements ParseWebPage {
+    public ParseCnet() {
 		// TODO Auto-generated constructor stub
 	}
+    
+	public static String websiteTitle(String url) throws IOException {
+		// TODO Auto-generated method stub
+        Document d=Jsoup.connect(url).timeout(6000).get();
+        System.out.println("Connected! \n");
+	return d.title();	
+	}
+
+    	@Override
+	public void findArticleBody() throws IOException{
+		// TODO Auto-generated method stub
+	}
 
 	@Override
-	public void findArticleBody() {
+	public void findSubtitle() throws IOException{
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void findSubtitle() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void findPros() {
+	public void findPros() throws IOException{
 		// TODO Auto-generated method stub
 
 	}
@@ -34,9 +50,16 @@ public class ParseCnet implements ParseWebPage {
 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
-
+Scanner input = new Scanner(System.in);
+    System.out.print("input url: ");
+		System.out.println("Title: " + websiteTitle(input.next()));
+		input.close();
 	}
 
+    @Override
+    public void websiteTitle() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
--- KHE2016/src/ParseWebPage.java
+++ KHE2016/src/ParseWebPage.java
@@ -1,15 +1,20 @@
-/**
- * 
+
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-package khe2016;
 
 /**
- * @author joshreichman
  *
+ * @author joshreichman
  */
 public interface ParseWebPage {
-void findArticleBody();
-void findSubtitle();
-void findPros();
-void findCons();
+    void websiteTitle()throws IOException;
+    void findArticleBody()throws IOException;
+    void findSubtitle()throws IOException;
+    void findPros()throws IOException;
+    void findCons()throws IOException;
 }
