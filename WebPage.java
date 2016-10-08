f817e14fa2fac504daee8894720cbde1740039c8
Emilio Lopez <eil11@case.edu>
2016-10-08 10:42:42 -0400

    WebPage has fields and documentation

--- WebPage.java
+++ WebPage.java
@@ -1,26 +1,26 @@
 
 /**
- * Abstract class that allows for standardization of fields for the unique 
- * structure of each publisher's HTML structure. 
+ * Abstract class that allows for standardization of fields for the unique
+ * structure of each publisher's HTML structure.
  * @author Emilio Lopez
  * *
  */
 public abstract class WebPage {
-    
+
     /** Web site publisher of the review. */
     private String publisher;
-    
+
     /** Article title. */
     private String articleTitle;
-    
+
     /** The author of the article. */
     private String author;
-    
+
     /** The review article body. */
     private String articleBody;
-    
+
     /** The URL of the review. */
     private String URL;
-    
-    
+
+
 }
