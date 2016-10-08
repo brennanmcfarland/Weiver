4aeb43a78f3cf4dee4723635c658e96d6e10e8e0
Emilio Lopez <eil11@case.edu>
2016-10-08 15:35:30 -0400

    First webpage class for CNET

---
+++ KHE2016/src/CNETPage.java
@@ -0,0 +1,25 @@
+
+import java.util.regex.*;
+
+/**
+ *
+ * @author ei_lo
+ */
+public class CNETPage extends WebPage {
+    
+    public CNETPage(String articleTitle, String articleBody){
+        
+        this.publisher = "CNET";
+        this.articleTitle = articleTitle;
+        this.articleBody = articleBody;
+        
+        
+    }
+    
+    // TODO: Void needs to be changed 
+    private void createPattern(){
+        
+    }
+    
+    
+}
