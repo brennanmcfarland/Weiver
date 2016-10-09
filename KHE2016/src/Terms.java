18f8174c0b938bf2ce316c65048b2c18122c13fe
Brennan <4brothersnosisters@gmail.com>
2016-10-08 21:30:07 -0400

    added jsonsimple parsing library and outlined terms

--- KHE2016/src/Terms.java
+++ KHE2016/src/Terms.java
@@ -1,6 +1,16 @@
 import java.util.Hashtable;
+import org.json.simple.*;
 
 class Terms {
-
-  private Hashtable termFrequency;
+    public FrequencyTable termFrequency; //holds terms and their frequencies
+    public Hashtable ignorePhrases; //holds words/phrases to ignore
+    
+    //parses a json file to load the phrases to ignore when finding frequencies
+    public void parseFileIgnorePhrases(String filename) {
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
